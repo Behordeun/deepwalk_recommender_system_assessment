5761c064eb91785d59c21997535512b914736f98
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-06-23 10:51:15 +0100

    Add __init__.py with default settings and metadata

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Author: Muhammad Abiodun SULAIMAN abiodun.msulaiman@gmail.com
+Date: 2025-06-22 21:23:05
+LastEditors: Muhammad Abiodun SULAIMAN abiodun.msulaiman@gmail.com
+LastEditTime: 2025-06-23 09:00:20
+FilePath: tests/__init__.py
+Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+"""
