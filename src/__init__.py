ea3fce848b2d0478f117ebfff87db2677f7bd861
Muhammad Abiodun SULAIMAN <abiodun.msulaiman@gmail.com>
2025-06-24 04:04:23 +0100

    Add initial __init__.py file with default settings description

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Author: Muhammad Abiodun SULAIMAN abiodun.msulaiman@gmail.com
+Date: 2025-06-24 03:50:47
+LastEditors: Muhammad Abiodun SULAIMAN abiodun.msulaiman@gmail.com
+LastEditTime: 2025-06-24 03:51:00
+FilePath: src/__init__.py
+Description: 这是默认设置,可以在设置》工具》File Description中进行配置
+"""
